91bd9afa1fa3530a4ff8de78a5b313411cf853b7
Anwealso <48880195+Anwealso@users.noreply.github.com>
2023-01-14 14:51:33 +1000

    Added part-finished tfjs deployment implementation

---
+++ deploy/index.js
@@ -0,0 +1,115 @@
+/**
+ * index.js
+ * 
+ * Alex Nicholson
+ * 14/01/2023
+ */
+
+/* -------------------------------------------------------------------------- */
+/*                                   IMPORTS                                  */
+/* -------------------------------------------------------------------------- */
+import * as tf from '/node_modules/@tensorflow/tfjs';
+import * as tfd from '/node_modules/@tensorflow/tfjs-data';
+import * as ui from './ui.js';
+
+
+/* -------------------------------------------------------------------------- */
+/*                                 GLOBAL VARS                                */
+/* -------------------------------------------------------------------------- */
+let webcam; // A webcam iterator that generates Tensors from the images from the webcam.
+let model; // The imported trained model
+let isPredicting = false;
+
+
+/* -------------------------------------------------------------------------- */
+/*                               EVENT LISTENERS                              */
+/* -------------------------------------------------------------------------- */
+document.getElementById('train').addEventListener('click', async () => {
+  isPredicting = false;
+});
+
+document.getElementById('predict').addEventListener('click', () => {
+  isPredicting = true;
+  predict();
+});
+
+
+/* -------------------------------------------------------------------------- */
+/*                                  FUNCTIONS                                 */
+/* -------------------------------------------------------------------------- */
+async function predict() {
+  /**
+   * Gets images from the webcam and runs the model on them to get the model's 
+   * predictions
+   */
+
+  ui.isPredicting();
+  while (isPredicting) {
+    // Capture the frame from the webcam.
+    const img = await getImage();
+
+    // Make a prediction through mobilenet
+    const predictions = model.predict(img);
+    // console.log(predictions);
+
+    // Returns the index with the maximum probability. This number corresponds
+    // to the class the model thinks is the most probable given the input.
+    const predictedClass = predictions.as1D().argMax();
+    const classId = (await predictedClass.data())[0];
+    
+    // console.log(predictedClass);
+    console.log(classId);
+
+    img.dispose();
+
+    ui.predictClass(classId);
+    await tf.nextFrame();
+  }
+  ui.donePredicting();
+}
+
+async function getImage() {
+  /**
+   * Captures a frame from the webcam and normalizes it between -1 and 1.
+   * Returns a batched image (1-element batch) of shape [1, w, h, c].
+   */
+  
+  const img = await webcam.capture();
+  const processedImg =
+      tf.tidy(() => img.expandDims(0).toFloat().div(127).sub(1));
+  img.dispose();
+  return processedImg;
+}
+
+
+/* -------------------------------------------------------------------------- */
+/*                                MAIN FUNCTION                               */
+/* -------------------------------------------------------------------------- */
+async function init() {
+  webcamConfig = {};
+  webcamConfig.facingMode = 'environment'; // show the front facing camera on mobile devices
+
+  try {
+    webcam = await tfd.webcam(document.getElementById('webcam'), webcamConfig);
+  } catch (e) {
+    console.log(e);
+    document.getElementById('no-webcam').style.display = 'block';
+  }
+  model = await tf.loadLayersModel('http://127.0.0.1:3000/tfjs_artifacts/model.json');
+
+  ui.init();
+
+  // Warm up the model. This uploads weights to the GPU and compiles the WebGL
+  // programs so the first time we collect data from the webcam it will be
+  // quick.
+  const screenShot = await webcam.capture();
+  model.predict(screenShot.expandDims(0));
+  screenShot.dispose();
+  
+  // Run the model immediately
+  isPredicting = true;
+  predict();
+}
+
+// Initialize the application.
+init();
---
+++ deploy/server.js
@@ -0,0 +1,28 @@
+const express = require('express'); // import express lib
+const path = require('path');
+
+const hostname = '127.0.0.1';
+const port = 3000; // set port to connect to server on  
+
+const app = express(); // create the express server
+
+app.use(express.static('dist')); // tell it where to find the public contents
+
+// app.set("views", "./src") // tell it where to find the views
+
+// Serve the model files
+app.use('/tfjs_artifacts', express.static(path.join(__dirname, 'web_model')))
+
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + "/" + 'index.html'));
+})
+
+app.get('/get_model', function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.sendFile(path.join(__dirname + "/web_model/" + 'model.json'));
+})
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+})
